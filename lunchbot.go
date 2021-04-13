cae0463ae551eaae160b944cf0841f66d2681d04
Thomas Qvidahl <thomas.qvidahl@evry.com>
2021-04-13 16:08:05 +0200

    🎨  Sends msg to Slack with URL only. Will display comic in Slack as intended. Also downloads comic to local file, but this isn't strictly  necessary.

--- lunchbot.go
+++ lunchbot.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -8,9 +10,79 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
+type SlackRequestBody struct {
+	Text string `json:"text"`
+}
+
 func main() {
+
+	webHookUrl, ok := getEnvironment("WEBHOOK_URL")
+	if !ok {
+		log.Fatal("No string")
+	}
+
+	fileName, url := getComic()
+
+	err1 := SendSlackNotification(webHookUrl, "Dagens Lunch "+url)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	err2 := downloadFile(url, fileName)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	fmt.Printf("File %s downloaded in current working directory", fileName)
+}
+
+func getEnvironment(varName string) (string, bool) {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.LookupEnv(varName)
+}
+
+func HowToSendSlackNotification() {
+	webHookUrl, _ := getEnvironment("WEBHOOK_URL")
+	_, url := getComic()
+	err := SendSlackNotification(webHookUrl, "Dagens Lunch "+url)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SendSlackNotification(webhookUrl string, msg string) error {
+
+	slackBody, _ := json.Marshal(SlackRequestBody{Text: msg})
+	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		return errors.New("Non-ok repsonse returned from Slack")
+	}
+	return nil
+}
+
+func getComic() (string, string) {
 	baseUrl := "https://tu.no"
 	date := time.Now().Format("2006-01-02")
 	comicId := "lunch"
@@ -18,12 +90,7 @@ func main() {
 	URL := baseUrl + "/?module=TekComics&service=image&id=" + comicId + "&key=" + date
 	// URL looks like this https://www.tu.no/?module=TekComics&service=image&id=lunch&key=2020-05-28
 
-	err := downloadFile(URL, fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("File %s downloaded in current working directory", fileName)
-
+	return fileName, URL
 }
 
 func downloadFile(URL, fileName string) error {
